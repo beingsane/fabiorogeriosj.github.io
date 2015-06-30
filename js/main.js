ec7560ff5ed1a23abe9a96e7f2e2f6a6c7d22bc7
Fábio Rogério SJ <fabio.rogerio.sj@gmail.com>
2015-06-30 15:59:07 -0300

    Post1

---
+++ js/main.js
@@ -0,0 +1,44 @@
+var app = angular.module('fabiorogeriosj', []);
+
+app.run(function($rootScope) {
+    $rootScope.exibirProfile=false;
+    $rootScope.mostrarProfile = function(){
+    	$rootScope.exibirProfile=!$rootScope.exibirProfile;
+    };
+    $rootScope.go = function(link){
+    	location.href=link;
+    }
+});
+
+app.controller('indexController', function($scope, $http) {
+    
+	//Codigo aqui
+
+	// $http.get('js/posts.json').success(function(data){
+	// 	console.log(data);
+	// });
+
+	$scope.posts = posts;
+
+});
+
+app.controller('categoriaController', function($scope, $http) {
+	$scope.categoria = location.hash.split("#/")[1];
+	$scope.posts = posts;
+});
+
+app.controller('postController', function($scope, $http) {
+	$scope.posts = posts;
+	$scope.post = {};
+	$scope.postKey = "";
+	for(i in location.pathname.split("/")){
+		if(location.pathname.split("/")[i].indexOf(".html")){
+			$scope.postKey = location.pathname.split("/")[i];
+		}
+	}
+	for(i in $scope.posts){
+		if($scope.postKey == $scope.posts[i].link){
+			$scope.post = $scope.posts[i];
+		}
+	}
+});
---
+++ js/posts.json.js
@@ -0,0 +1,10 @@
+var posts = [
+	{
+		"categoria":"Hospedagem",
+		"imagem":"post1.png",
+		"titulo":"Publicando um site sem pagar nada ou quase nada!",
+		"data":"30/06/2015",
+		"descricao":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis, arcu in hendrerit consequat, justo sapien pretium sapien, dictum posuere felis turpis non ligula. Sed sit amet mi non lectus sagittis pellentesque nec ac tortor. Aliquam quis dictum nunc, a volutpat ligula. Donec semper enim ut nisl luctus egestas. Proin ante magna, maximus at ante vitae, laoreet dictum eros.",
+		"link":"publicando-um-site-sem-pagar-nada-ou-quase-nada.html"
+	}
+]
