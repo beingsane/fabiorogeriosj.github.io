99781394987504f95a13d5f3f61a1d2f70200792
Fábio Rogério SJ <fabio.rogerio.sj@gmail.com>
2015-07-01 16:19:10 -0300

    post completo

--- js/posts.json.js
+++ js/posts.json.js
@@ -5,6 +5,6 @@ var posts = [
 		"titulo":"Publicando um site sem pagar nada!",
 		"data":"30/06/2015",
 		"descricao":"Quem já não se deparou com a seguinte situação: Preciso publicar um site, bem simples e estático, porem acho carro pagar R$ 19,90 por mês! Neste post vou explicar de forma prática e simples como hospedar um site sem pagar nada ou quase nada :)",
-		"link":"publicando-um-site-sem-pagar-nada-ou-quase-nada.html"
+		"link":"publicando-um-site-sem-pagar-nada.html"
 	}
 ]
