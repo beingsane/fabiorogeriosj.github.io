c2e92961b1386e578f45945a167bf1f0945a979c
Fábio Rogério SJ <fabio.rogerio.sj@gmail.com>
2015-07-01 16:18:14 -0300

    post completo

--- js/posts.json.js
+++ js/posts.json.js
@@ -2,9 +2,9 @@ var posts = [
 	{
 		"categoria":"Hospedagem",
 		"imagem":"post1.png",
-		"titulo":"Publicando um site sem pagar nada, ou quase nada!",
+		"titulo":"Publicando um site sem pagar nada!",
 		"data":"30/06/2015",
-		"descricao":"Quem já não se deparou com a seguinte situação: Preciso publicar um site, bem simples e estático, porem acho carro pagar R$ 19,90 por mês! Neste post vou explicar de forma prática e simples como hospedar um site sem pagar nada ou quase nada :), dependendo da extensão do domínio!",
+		"descricao":"Quem já não se deparou com a seguinte situação: Preciso publicar um site, bem simples e estático, porem acho carro pagar R$ 19,90 por mês! Neste post vou explicar de forma prática e simples como hospedar um site sem pagar nada ou quase nada :)",
 		"link":"publicando-um-site-sem-pagar-nada-ou-quase-nada.html"
 	}
 ]
