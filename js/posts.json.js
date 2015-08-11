5d3ddadbceb2e75f1158e3f2195140e8ce88d7fc
Fábio Rogério SJ <fabio.rogerio.sj@gmail.com>
2015-08-11 20:41:48 -0300

    Erro relatado pelos alunos

--- js/posts.json.js
+++ js/posts.json.js
@@ -4,7 +4,7 @@ var posts = [
 		"imagem":"post1.png",
 		"titulo":"Publicando um site sem pagar nada!",
 		"data":"30/06/2015",
-		"descricao":"Quem já não se deparou com a seguinte situação: Preciso publicar um site, bem simples e estático, porem acho carro pagar R$ 19,90 por mês! Neste post vou explicar de forma prática e simples como hospedar um site sem pagar nada ou quase nada :)",
+		"descricao":"Quem já não se deparou com a seguinte situação: Preciso publicar um site, bem simples e estático, porém acho caro pagar R$ 19,90 por mês! Neste post vou explicar de forma prática e simples como hospedar um site sem pagar nada ou quase nada :)",
 		"link":"publicando-um-site-sem-pagar-nada.html"
 	}
 ]
